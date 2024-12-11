4a18c9f357a3656480dcb1230ffa47204e783616
Sergey Kazenyuk <kazenyuk@pm.me>
2024-12-11 19:52:04 +0300

    modules: key for the module generated by mkRemovedOptionModule

--- lib/modules.nix
+++ lib/modules.nix
@@ -1632,6 +1632,7 @@ let
     optionName: replacementInstructions:
     { options, ... }:
     {
+      key = "removedOptionModule#" + concatStringsSep "_" optionName;
       options = setAttrByPath optionName (mkOption {
         visible = false;
         apply =
